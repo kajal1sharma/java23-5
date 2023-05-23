5b63678f9171c4f7134120778483294a2b056c12
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-23 13:39:54 +0530

    ..

---
+++ Test.java
@@ -0,0 +1,16 @@
+class Test{
+
+
+    //function created  => with name of main 
+    public static void main(String[] args) {
+        
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        
+    }
+
+
+}
