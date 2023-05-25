0d42ac3b35fff3b2fc99b071efab1aa81e93a0f0
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-25 14:00:29 +0530

    Identifiers

---
+++ Identifiers.java
@@ -0,0 +1,35 @@
+public class Identifiers {
+ 
+    public static void main(String[] args) {
+        
+
+        byte b = -128;
+        b=45;
+        b=23+45;
+
+        byte maxPos = Byte.MAX_VALUE;
+        byte maxNeg = Byte.MIN_VALUE;
+        System.out.println(maxPos);
+        System.out.println(maxNeg);
+
+        //variables => whose value can vary
+
+        // Data types
+
+        // byte
+        // short
+        // int
+        // long 
+        // float
+        // Double
+
+
+        // char
+
+
+        // boolean
+
+        
+
+    }
+}
