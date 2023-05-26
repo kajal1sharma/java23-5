eb8182c87e6bd53be8d00dddb5e076f1aa1b80d3
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-26 14:25:41 +0530

    ..

--- Identifiers.java
+++ Identifiers.java
@@ -1,16 +1,73 @@
+
 public class Identifiers {
  
     public static void main(String[] args) {
+
+
+     
+
+        float floatNum;
+        floatNum=56.78f;
+
+        float maxValue = Float.MAX_VALUE;
+        System.out.println(maxValue);
+
+        double d= 56.78;
+
+
+        //multi line comments /*  */
+       /*  int num2;//declaration
+        num2=56; //initialization
+
+        int num3=1;
+
+        int firstNumber;
+        int first_number;
+        int $firstnumber;
+        // int 134firstNumber;(wrong)
+        int first123number;
+        int _first_number;
+        //_ $ alphabet lower and upper and digits(not at starting)
+*/
+
+
+
+
+
+
+        // int num1= 123;// 4 byte
+
+        // int intMaxValue = Integer.MAX_VALUE;
+        // int intMinValue = Integer.MIN_VALUE;
         
 
-        byte b = -128;
-        b=45;
-        b=23+45;
+        // System.out.println(intMaxValue);
+        // System.out.println(intMinValue);
 
-        byte maxPos = Byte.MAX_VALUE;
-        byte maxNeg = Byte.MIN_VALUE;
-        System.out.println(maxPos);
-        System.out.println(maxNeg);
+        // long minValueLong = Long.MIN_VALUE;//8bytes
+        // long maxValueLong = Long.MAX_VALUE; 
+
+        // System.out.println(minValueLong);
+        // System.out.println(maxValueLong);
+
+
+        // short num =34;//2 byte
+
+
+        // short tom= Short.MAX_VALUE;
+        // System.out.println(tom);
+        // short jerry =Short.MIN_VALUE;
+    
+
+        //byte b = -128;// 1 byte
+        // b=45;
+        // b=23+45;
+
+        // byte maxPos = Byte.MAX_VALUE;
+        // byte maxNeg = Byte.MIN_VALUE;
+
+        // System.out.println(maxPos);
+        // System.out.println(maxNeg);
 
         //variables => whose value can vary
 
@@ -32,4 +89,5 @@ public class Identifiers {
         
 
     }
+
 }
