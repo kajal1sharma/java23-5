c76c14f4ce48a2a913a468f594b8e516b937a0cb
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-30 14:24:35 +0530

    ..

--- Literals.java
+++ Literals.java
@@ -1,17 +1,43 @@
 public class Literals {
     public static void main(String[] args) {
-        boolean b = true;
-        b=false;
-        boolean var1, var2;
-        boolean b1 = true , b2=false;
-        int i1=12, i2=34, i3, i4=90, i5;
-        System.out.println(i1);
-        // System.out.println(i3);//initialization is must
-        int a =90;
-        char c='a';
-        char ch =97;
-        int character =ch;
-        System.out.println(ch);
+
+
+        char c = 97;
+         System.out.println(c); 
+         char c2 = '*';
+         System.out.println(c2);
+         char c3 ='\u0014';
+         System.out.println(c3);
+
+        // float f=878.778f;
+        // long num =938_492_90323_3234L;
+        
+
+        // System.out.println(num);
+
+        // int decimalnum = 99;//decimal  (0-9) base 10
+        // int octalnum = 0327; //ocatal representation (0-7) base 8
+        // int hexNum = 0X1224fd  ; //0-9 a-f ,a/A,b/B,c/C,d/D,E/e,F/f base 16
+        // int hexNum2 =0xfC23D;
+        // int binNum = 0b1010101010;// (0,1)base 2
+        
+
+        // System.out.println(decimalnum);
+        // System.out.println(octalnum);
+        // System.out.println(hexNum);
+        // System.out.println(binNum);
+        // boolean b = true;
+        // b=false;
+        // boolean var1, var2;
+        // boolean b1 = true , b2=false;
+        // int i1=12, i2=34, i3, i4=90, i5;
+        // System.out.println(i1);
+        // // System.out.println(i3);//initialization is must
+        // int a =90;
+        // char c='a';
+        // char ch =97;
+        // int character =ch;
+        // System.out.println(ch);
         //short s=ch
 
         //char ch2= 2 bytes;
