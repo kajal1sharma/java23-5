f32fafac6751265a4320596c11cf26b7b35bc3bc
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-31 13:11:38 +0530

    ..

---
+++ Array.java
@@ -0,0 +1,37 @@
+public class Array {
+    public static void main(String[] args) {
+        
+        
+        int marksStudent1 = 90;
+        int marksStudent2 = 89;
+        int marksStudent3 =56;
+        int marksStudent4 =67;
+        int marksStudent5 =78;
+        int marksStudent6 =90;
+        int marksStudent7 =45;
+        int marksStudent8=33;
+
+        int sumOfAllStudents =marksStudent1+marksStudent2+marksStudent3+marksStudent4+marksStudent5+marksStudent7+marksStudent6+marksStudent8;
+
+
+        //array can store similar type huge amount of data under one name with the help indices
+
+        int []marks = new int[10];
+        marks[0]=90;
+        marks[1]=98;
+        marks[2]=78;
+        marks[3]=89;
+        marks[4]=90;
+        marks[5]=78;
+        System.out.println(marks[4]);
+         
+
+
+
+
+
+
+        
+
+    }
+}
