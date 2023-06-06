7c5ad05b3fbefcafeda8c0e2105756c2f611b444
brainmentorspvtltd <kajal1231995@gmail.com>
2023-06-06 13:22:53 +0530

    ..

---
+++ Array3.java
@@ -0,0 +1,28 @@
+public class Array3{
+
+    public static void main(String[] args) {
+        
+        
+        int []arr = new int[10];
+        int arr2[] = {1,2,3,4,5,6,7,8};
+
+        
+
+
+        // System.out.println(arr2[0]);
+        // System.out.println(arr2[1]);
+        // System.out.println(arr2[2]);
+        // System.out.println(arr2[3]);
+        // System.out.println(arr2[4]);
+        // System.out.println(arr2[5]);
+        // System.out.println(arr2[6]);
+        // System.out.println(arr2[7]);
+
+        // for( int ele : arr2){
+        //     System.out.println(ele);
+        // }
+
+
+
+    }
+}
---
+++ ForLoop.java
@@ -0,0 +1,24 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        
+
+        //conditionals operators 
+        //==, < ,> <= ,>=
+
+        int a=11;
+        int b=11;
+
+        // boolean result = a==b;
+        //boolean result = a <= b;
+        boolean result  =  a >= b;
+        System.out.println(result);
+
+        // int a= 19;
+        // int b=10;
+
+        // boolean result =a < b; 
+        // boolean result2 =a > b;
+        // System.out.println(result);
+        // System.out.println(result2);
+    }
+}
