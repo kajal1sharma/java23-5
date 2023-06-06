cbc8fcc30f9d426812c15653dbf1f70a9c974caf
brainmentorspvtltd <kajal1231995@gmail.com>
2023-06-06 13:40:40 +0530

    ..

---
+++ Operators.java
@@ -0,0 +1,39 @@
+public class Operators {
+    public static void main(String[] args) {
+        
+
+        int a=90;
+        int b=90;
+        boolean result1=a>b;
+        boolean result2=a<b;
+        boolean result3=a==b;
+        boolean result4=a>=b;
+        boolean result5=a<=b;
+        boolean result6 = a!=b;
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
+        System.out.println(result6);
+
+        //conditionals operators 
+        //==, < ,> <= ,>=
+
+        // int a=11;
+        // int b=11;
+
+        // // boolean result = a==b;
+        // //boolean result = a <= b;
+        // boolean result  =  a >= b;
+        // System.out.println(result);
+
+        // int a= 19;
+        // int b=10;
+
+        // boolean result =a < b; 
+        // boolean result2 =a > b;
+        // System.out.println(result);
+        // System.out.println(result2);
+    }
+}
