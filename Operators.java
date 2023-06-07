8e32bb663bf8f15375d1cd2d5b9fa6ee9d06a891
brainmentorspvtltd <kajal1231995@gmail.com>
2023-06-07 13:02:44 +0530

    ..

--- Operators.java
+++ Operators.java
@@ -2,23 +2,48 @@ public class Operators {
     public static void main(String[] args) {
         
 
-        int a=90;
-        int b=90;
-        boolean result1=a>b;
-        boolean result2=a<b;
-        boolean result3=a==b;
-        boolean result4=a>=b;
-        boolean result5=a<=b;
-        boolean result6 = a!=b;
-        System.out.println(result1);
-        System.out.println(result2);
-        System.out.println(result3);
-        System.out.println(result4);
-        System.out.println(result5);
-        System.out.println(result6);
+        //relational operator
+
+        //&&(And) , ||(Or) ,!(Not)
+
+         boolean bool1 =false;
+         boolean bool2 =false;
+
+         boolean result = bool1 || bool2;
+         System.out.println(result);
+
+
+        // boolean bool1=false;
+        // boolean bool2=false;
+
+        // boolean result  =  bool1 && bool2;
+        // System.out.println(result);
+
+
+
+
+
+
+
+
+
+        // int a=90;
+        // int b=90;
+        // boolean result1=a>b;
+        // boolean result2=a<b;
+        // boolean result3=a==b;
+        // boolean result4=a>=b;
+        // boolean result5=a<=b;
+        // boolean result6 = a!=b;
+        // System.out.println(result1);
+        // System.out.println(result2);
+        // System.out.println(result3);
+        // System.out.println(result4);
+        // System.out.println(result5);
+        // System.out.println(result6);
 
         //conditionals operators 
-        //==, < ,> <= ,>=
+        //==, < ,> <= ,>=, !=
 
         // int a=11;
         // int b=11;
