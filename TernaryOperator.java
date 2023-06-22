06decfe8d735d9eb58f313df733862279f4d3af6
kajal1sharma <kajal1231995@gmail.com>
2023-06-22 14:21:18 +0530

    Create TernaryOperator.java

---
+++ TernaryOperator.java
@@ -0,0 +1,25 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+
+        int a = 7667;
+
+        String str = a % 2 == 0 ? "this number is even " : "this number is odd";
+        System.out.println(str);
+
+        // int result = a == 90 ? 4 + 5 : 6 + 7;
+        // System.out.println(result);
+        // String str = a == 89 ? "this is right" : "this is not right";
+        // System.out.println(a == 90 ? 4 + 5 : "dfjhgjsd");
+
+        // boolean rain = true;
+        // boolean umbrella = true;
+        // String ans = rain == true && umbrella == false ? "you cannot go out " : "you
+        // ca go out";
+        // System.out.println(ans);
+
+        // exp1?exp2:exp3
+
+        // operands+operand= A+B =(A+B)+(C+D)
+
+    }
+}
