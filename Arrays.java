94c3118c6428cf75b1026eb5e65f12aba5aa6f4d
kajal1sharma <kajal1231995@gmail.com>
2023-06-22 14:18:38 +0530

    Create Arrays.java

---
+++ Arrays.java
@@ -0,0 +1,18 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int[] nums = new int[5];
+        nums[0] = 67;
+        nums[1] = 90;
+        nums[2] = 78;
+        nums[3] = 70;
+        nums[4] = 34;
+        // nums[5] = 78;
+
+        System.out.println(nums[0]);
+        System.out.println(nums[1]);
+        System.out.println(nums[2]);
+        System.out.println(nums[3]);
+        System.out.println(nums[4]);
+        // System.out.println(nums[5]);
+    }
+}
