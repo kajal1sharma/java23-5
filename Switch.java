3399710d82cd3004b14e7f7305c66b76f89fed18
kajal1sharma <kajal1231995@gmail.com>
2023-06-22 14:20:34 +0530

    Create Switch.java

---
+++ Switch.java
@@ -0,0 +1,67 @@
+
+//whenver we want to use Scanner class we have to import it
+import java.util.Scanner;
+
+// class Abc{
+
+// }
+
+public class Switch {
+    public static void main(String[] args) {
+
+        // Abc obj =new Abc();
+
+        // Scanner sc = new Scanner(System.in);
+
+        // int a = sc.nextInt();
+
+        String a = "Monday";
+        switch (a) {
+            case "Monday":
+                System.out.println("Monday");
+                break;
+            case "Tueday":
+                System.out.println("Tuesday");
+                break;
+            case "Wednesday":
+                System.out.println("Wednesday");
+                break;
+            case "Thurday":
+                System.out.println("Thursday");
+            case "Friday":
+                System.out.println("Friday");
+            case "Saturday":
+                System.out.println("Saturday");
+            case "sunday":
+                System.out.println("Sunday");
+            default:
+                System.out.println("wrong input");
+
+        }
+
+        // int a;
+        // System.out.println("enter a number");
+        // a = sc.nextInt();
+        // System.out.println("enter boolean value");
+        // boolean b = sc.nextBoolean();
+        // System.out.println("enter a float value");
+        // float f = sc.nextFloat();
+        // System.out.println("enter a long value");
+        // long l = sc.nextLong();
+        // System.out.println("ennter a string");
+        // String s2 = sc.nextLine();
+        // String s = sc.next();
+        // System.out.println(s2);
+        // System.out.println(s);
+        // char c = sc.next().charAt(0);
+        // System.out.println(c);
+
+        // int a = sc.nextInt();
+        // sc.nextLine();
+        // String s = sc.nextLine();
+
+        // System.out.println(a);
+        // System.out.println(s);
+
+    }
+}
