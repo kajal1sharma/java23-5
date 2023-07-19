224608dac9f64515584a1c33302b50b2d1aace36
kajal1sharma <kajal1231995@gmail.com>
2023-07-19 14:13:15 +0530

    Create abstractKey.java

---
+++ abstractKey.java
@@ -0,0 +1,40 @@
+
+abstract class Book {
+    int page;
+    int cost;
+    String title;
+
+    void getInfo() {
+        System.out.println(this.page);
+        System.out.println(this.cost);
+        System.out.println(this.title);
+    }
+
+    abstract void aboutTheBook();
+
+    abstract void getcost();
+}
+
+class ScienceAndTech extends Book {
+    int x;
+    int y;
+
+    void aboutTheBook() {
+        System.out.println("abnout the book");
+    }
+
+    void getcost() {
+        System.out.println("get the cost");
+    }
+
+}
+
+public class abstractKey {
+    public static void main(String[] args) {
+        // Book b=new Book();
+        ScienceAndTech sc = new ScienceAndTech();
+        sc.aboutTheBook();
+        sc.cost = 90;
+
+    }
+}
