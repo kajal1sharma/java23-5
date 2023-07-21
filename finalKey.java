dc74e81eeb62a3b284594d7e2efa8a71c62a7d0c
brainmentorspvtltd <kajal1231995@gmail.com>
2023-07-21 14:16:57 +0530

    ..

--- finalKey.java
+++ finalKey.java
@@ -1,17 +1,32 @@
-import java.security.Principal;
-
-class College{
-
-    static final String principal;
-    static {
-       principal="mk singh";
-       //computation
-       a=45*89;
-       a=90;
-       a=467;
-       final int c=90;
-
+class University{
+    int x;
+    int y;
+    int z;
+    final void printInfo(){
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
     }
+    void readInfo(){
+        System.out.print(x);
+         System.out.println(y);
+        System.out.println(z);
+    }
+}
+final class College extends University{
+
+    static final String principal="mk singh";
+    
+    // static {
+    //    principal="mk singh";
+       
+    //    //computation
+    //    a=45*89;
+    //    a=90;
+    //    a=467;
+    //    final int c=90;
+
+    // }
 
     static int a;
     final String name;
@@ -23,10 +38,22 @@ class College{
     College(String name){
         this.name=name;
     }
+    // void printInfo(){
 
+    // }
+    void readInfo(){
+         System.out.println(y);
+        System.out.println(z);
+        System.out.println(name);
+        System.out.println(Naac);
+        System.out.println(principal);
+    }
     //you cannot initialize final data member anywhere else except for
     // at the declaration or in the construct
 }
+// class Abc extends College{
+
+// }
 public class finalKey {
     public static void main(String[] args){
         final int d=90;
