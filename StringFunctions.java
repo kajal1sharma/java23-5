e63732b8773148f055f109c365db401f8da7938f
brainmentorspvtltd <kajal1231995@gmail.com>
2023-08-02 14:36:57 +0530

    ..

--- StringFunctions.java
+++ StringFunctions.java
@@ -17,6 +17,22 @@
         }
     }
 
+    static boolean checkPalindrome2(String str){
+
+        boolean flag =true;;
+        for(int i=0,j=str.length()-1;i<j;i++,j--){
+            if(str.charAt(i)==str.charAt(j)){
+
+            }
+            else{
+                flag =false;
+                break;
+            }
+        }
+
+        return flag;
+
+    }
 
 public static void main(String[] args) {
 
