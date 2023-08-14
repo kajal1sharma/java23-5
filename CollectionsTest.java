fb3a9245ed5555ffdc33a4f6a155c5ad61557716
kajal <kajal@gmail.com>
2023-08-14 14:14:25 +0530

    ..

---
+++ CollectionsTest.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class CollectionsTest {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(23);
+        list.add(45);
+        list.add(90);
+
+       
+        System.out.println(list);
+
+        
+        
+    }
+    
+}
--- StringFunction2.java
+++ StringFunction2.java
@@ -2,16 +2,12 @@ import java.util.Arrays;
 
 public class StringFunction2 {
     public static void main(String[] args) {
-
-
-
-
-        StringBuilder str = new StringBuilder("abcddsfajshjfhsdhfdsjafjshadf");
-        str.append(str);
-        str.append(str);
-        System.out.println(str.length());
-        System.out.println(str.capacity());
-        System.out.println(str.reverse());
+        // StringBuilder str = new StringBuilder("abcddsfajshjfhsdhfdsjafjshadf");
+        // str.append(str);
+        // str.append(str);
+        // System.out.println(str.length());
+        // System.out.println(str.capacity());
+        // System.out.println(str.reverse());
         // char []ch = {'a','b','c','c'};
         // String str2 = new String(ch);
         // str=str.append("strgndf");
