96960396aa229f2a871bb0918ad28fcdd8780c21
kajal <kajal@gmail.com>
2023-08-16 14:31:57 +0530

    ..

--- CollectionsTest.java
+++ CollectionsTest.java
@@ -1,17 +1,30 @@
 import java.util.ArrayList;
-
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 public class CollectionsTest {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<Integer>();
-        list.add(23);
+        LinkedList<Integer> list2=new LinkedList<Integer>();
         list.add(45);
-        list.add(90);
-
-       
+        list.add(475);
+        list.add(145);
+        
+        list2.add(56);
+        list2.add(506);
+        list2.add(560);
         System.out.println(list);
+        System.out.println(list2);
+        // list.add(23);
+        // list.add(45);
+        // list.add(90);
+        // System.out.println(list.size());
+       
+        // System.out.println(list);
 
-        
-        
+        List ref= new ArrayList<Integer>();
+        Collection c= new ArrayList<>();
+;        
     }
     
 }
