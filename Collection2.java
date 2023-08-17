c04ff8f6109549969d94bd8c1f6f6553c9f55966
kajal <kajal@gmail.com>
2023-08-17 14:31:50 +0530

    ..

--- Collection2.java
+++ Collection2.java
@@ -1,22 +1,55 @@
 import java.lang.Integer;
 import java.util.ArrayList;
+import java.util.Stack;
+
+class Test10{
+    int a;
+    int b;
+    Test10(int a, int b){
+        this.a=a;
+        this.b=b;
+    }
+}
+
 //all the classes of lang package are by default imported into your program 
 class Collection2{
     public static void main(String[] args) {
+        Test10 a= new Test10(10, 20);
+        Test10 b= new Test10(30, 40);
+        Test10 c= new Test10(50, 60);
+        Test10 d= new Test10(70, 80);
 
+        Stack<Test10> st = new Stack<Test10>();
+        st.push(a);
+         st.push(b);
+          st.push(c);
+           st.push(d);
 
-        int []arr=new int[10];
-        ArrayList<Integer> arrlist = new ArrayList<Integer>();
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        list.add(11);
-        list.add(12);
-        list.add(13);
-        arrlist.add(23);
-        arrlist.add(34);
-        arrlist.add(45);
-        arrlist.add(56);
-        arrlist.add(34);
-        System.out.println(arrlist.get(3));
+           Test10 ref = st.peek();
+           System.out.println(ref.a+"   "+ref.b);
+
+        //Car /Huma
+        // Stack<Integer> st = new Stack<Integer>();
+        // st.push(34);
+        // st.push(45);
+        // st.push(67);
+        // st.push(90);
+        // st.pop();
+        // st.pop();
+        // System.out.println(st.peek());
+        // System.out.println(st);
+       // int []arr=new int[10];
+        // ArrayList<Integer> arrlist = new ArrayList<Integer>();
+        // ArrayList<Integer> list = new ArrayList<Integer>();
+        // list.add(11);
+        // list.add(12);
+        // list.add(13);
+        // arrlist.add(23);
+        // arrlist.add(34);
+        // arrlist.add(45);
+        // arrlist.add(56);
+        // arrlist.add(34);
+        // System.out.println(arrlist.get(3));
         // System.out.println(arrlist.)
         // System.out.println(arrlist);
         // System.out.println(arrlist.size());
