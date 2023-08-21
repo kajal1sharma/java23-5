bec196dfdb09ffe047935eb3c9afd834f42d72eb
kajal <kajal@gmail.com>
2023-08-21 14:31:38 +0530

    ..

--- Collection2.java
+++ Collection2.java
@@ -1,6 +1,11 @@
 import java.lang.Integer;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
+import java.util.TreeSet;
 
 class Test10{
     int a;
@@ -14,19 +19,80 @@ class Test10{
 //all the classes of lang package are by default imported into your program 
 class Collection2{
     public static void main(String[] args) {
-        Test10 a= new Test10(10, 20);
-        Test10 b= new Test10(30, 40);
-        Test10 c= new Test10(50, 60);
-        Test10 d= new Test10(70, 80);
 
-        Stack<Test10> st = new Stack<Test10>();
-        st.push(a);
-         st.push(b);
-          st.push(c);
-           st.push(d);
 
-           Test10 ref = st.peek();
-           System.out.println(ref.a+"   "+ref.b);
+
+        TreeSet<Integer>  h= new TreeSet<>();
+        h.add(67);
+        h.add(90);
+        h.add(80);
+        h.add(45);
+        h.add(30);
+        h.add(21);
+        System.out.println(h);
+
+        // HashMap<Integer, String> hm = new HashMap<>();
+        // hm.put(23, "neena");
+        // hm.put(45,"priya");
+        // hm.put(67,"tiya");
+        // hm.put(null,"ajdshd");
+        // System.out.println(hm.containsKey(23));
+        // System.out.println(hm.get(23));
+        //  System.out.println(hm);
+        // hm.put(67,"shivani");
+
+        // System.out.println(hm);
+
+ 
+        // Queue<Integer> queue = new LinkedList<Integer>();
+        // queue.add(23);
+        // queue.add(90);
+        // queue.add(70);
+    
+        // System.out.println(queue.peek());
+        // System.out.println(queue.poll());
+        // System.out.println(queue);
+        // System.out.println(queue.remove());
+
+
+
+        //  ArrayList<Integer> arr2 =new ArrayList<>(40);
+        // arr2.add(23);
+        // arr2.add(23);
+        // arr2.add(null);
+        // arr2.add(90);
+        // arr2.add(45);
+        // System.out.println(arr2);
+        // HashSet<Integer> arr3 =new HashSet<Integer>();
+        // arr3.add(23);
+        // arr3.add(23);
+        // arr3.add(null);
+        // arr3.add(190);
+        // arr3.add(45);
+        // System.out.println(arr3);
+        // arr2.add(23);
+        // arr2.add(33);
+        // arr2.add(43);
+        // Stack<Integer> st= new Stack<Integer>();
+        // st.push(45);
+        // st.push(55);
+        // st.push(65);
+        // ArrayList<Integer> arr = new ArrayList<>(st);
+        // System.out.println(arr);
+
+        // Test10 a= new Test10(10, 20);
+        // Test10 b= new Test10(30, 40);
+        // Test10 c= new Test10(50, 60);
+        // Test10 d= new Test10(70, 80);
+
+        // Stack<Test10> st = new Stack<Test10>();
+        // st.push(a);
+        // st.push(b);
+        // st.push(c);
+        // st.push(d);
+
+        //    Test10 ref = st.peek();
+        //    System.out.println(ref.a+"   "+ref.b);
 
         //Car /Huma
         // Stack<Integer> st = new Stack<Integer>();
